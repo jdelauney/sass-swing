8c9b5f9619f5add8b84602839dcdb0fe46a7aabf
jdelauney <delauney.jerome@gmail.com>
2022-08-05 17:17:24 +0200

    feat(components/theme-switcher): Add theme switcher css rules and js script for switch between light and dark theme

---
+++ src/scripts/components/theme-switcher.js
@@ -0,0 +1,46 @@
+import CustomComponent from "../core/CustomComponent";
+
+export default class ThemeSwitcher extends CustomComponent{
+
+	/**
+	 * @param {HTMLElement} element
+	 * @param {String} localStorageThemeModeKey
+	 */
+	constructor(element, localStorageThemeModeKey) {
+		super(element)
+		this.localStorageThemeModeKey = localStorageThemeModeKey;
+		this.htmlRoot = document.querySelector("html");
+		this.currentTheme = window.localStorage.getItem(this.localStorageThemeModeKey);
+		this.prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+		if (this.currentTheme !== null) {
+			this._toggleTheme(this.currentTheme);
+		} else {
+			if (this.prefersDarkScheme.matches) {
+				this._toggleTheme("dark");
+			}
+		}
+
+		this.onSwitcherClick = this._handlerSwitchClick.bind(this);
+		this.element.addEventListener("click",this.onSwitcherClick);
+	}
+
+	_toggleTheme(themeMode) {
+		this.currentTheme = themeMode;
+		this.htmlRoot.setAttribute("data-theme", this.currentTheme);
+		window.localStorage.setItem(this.localStorageThemeModeKey, this.currentTheme);
+	}
+
+	_handlerSwitchClick() {
+		const newThemeMode = (this.htmlRoot.getAttribute("data-theme") !== "light") ? "light" : "dark";
+		this._toggleTheme(newThemeMode);
+	}
+
+	static bind(localStorageThemeModeKey = "swing-theme-mode") {
+		return Array.from(document.querySelectorAll("[data-js-themeSwitcher]")).map(
+			(element) => {
+				return new ThemeSwitcher(element, localStorageThemeModeKey);
+			}
+		);
+	}
+}
