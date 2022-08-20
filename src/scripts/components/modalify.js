16c0c2525bf6e30e1e8c103f07da1e5aac0e8f97
jdelauney <delauney.jerome@gmail.com>
2022-08-20 18:12:45 +0200

    fix: Fix various detected problems during review

--- src/scripts/components/modalify.js
+++ src/scripts/components/modalify.js
@@ -1,112 +1,104 @@
-import {waitForDOMEvent} from "../core/helpers/domUtils.js";
-import CustomComponent from "../core/CustomComponent.js";
+import { waitForDOMEvent } from '../core/helpers/domUtils.js';
+import CustomComponent from '../core/CustomComponent.js';
 
+export default class Modalify extends CustomComponent {
+  /**
+   * Paramètres des notifications cf _defaultConfig
+   * @type {Object}
+   * @private
+   */
+  _config = {};
 
-export default class Modalify extends CustomComponent{
+  /**
+   * Code de retour du bouton cliqué
+   * @type {String}
+   * @private
+   */
+  _modalResult = '';
 
-	/**
-	 * Paramètres des notifications cf _defaultConfig
-	 * @type {Object}
-	 * @private
-	 */
-	_config = {};
+  /**
+   * @param {HTMLDialogElement, String} element
+   */
+  constructor(element) {
+    super(element);
+    try {
+      this.buttons = this.element.querySelectorAll('button');
+      this.buttons.forEach((btn) => {
+        if (btn.hasAttribute('value') || btn.hasAttribute('data-modal-result')) {
+          btn.addEventListener('click', this._buttonHandlerClick.bind(this));
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
 
-	/**
-	 * Code de retour du bouton cliqué
-	 * @type {String}
-	 * @private
-	 */
-	_modalResult = "";
+    this.element.addEventListener('close', () => {
+      const htmlRoot = document.querySelector('html');
+      htmlRoot.classList.remove('disabled-scroll');
+    });
+  }
 
-	/**
-	 * @param {HTMLDialogElement, String} element
-	 */
-	constructor(element) {
-		super(element);
-		try {
-			this.buttons = this.element.querySelectorAll("button");
-			this.buttons.forEach((btn) => {
-				if ((btn.hasAttribute("value")) || (btn.hasAttribute("modal-result"))) {
-					btn.addEventListener("click", this._buttonHandlerClick.bind(this));
-				}
-			});
-		} catch (error) {
-			console.error(error);
-			return null;
-		}
+  _buttonHandlerClick(evt) {
+    const target = evt.currentTarget;
+    this._modalResult = '';
+    if (target.hasAttribute('value')) {
+      this._modalResult = target.value;
+    } else if (target.hasAttribute('data-modal-result')) {
+      this._modalResult = target.getAttribute('data-modal-result');
+    }
+  }
 
-		this.element.addEventListener('close', () => {
-			const htmlRoot =document.querySelector('html');
-			htmlRoot.classList.remove('disabled-scroll');
-		})
-	}
+  _animationsCompleted() {
+    return Promise.allSettled(this.element.getAnimations().map((animation) => animation.finished));
+  }
 
-	_buttonHandlerClick(evt) {
-		const target = evt.currentTarget;
-		this._modalResult = "";
-		if (target.hasAttribute("value")) {
-			this._modalResult = target.value;
-		}
-		else if (target.hasAttribute("modal-result")) {
-			this._modalResult = target.getAttribute("modal-result");
-		}
+  async close() {
+    await this._animationsCompleted();
+    this.element.close();
+  }
 
-	}
+  async showModal() {
+    const htmlRoot = document.querySelector('html');
+    htmlRoot.classList.add('disabled-scroll');
+    this.element.showModal();
+    const btns = [...this.buttons];
+    const buttonsRace = btns.map((btn) => {
+      return waitForDOMEvent(btn, 'click');
+    });
 
-	_animationsCompleted() {
-		return Promise.allSettled(
-			this.element.getAnimations().map(animation =>
-				animation.finished))
-	}
+    await Promise.race(buttonsRace);
 
-	async close() {
-		await this._animationsCompleted();
-		this.element.close();
-	}
+    await this.close();
 
-	async showModal() {
-		const htmlRoot =document.querySelector('html');
-		htmlRoot.classList.add('disabled-scroll');
-		this.element.showModal();
-		const btns = [...this.buttons]
-		const buttonsRace = btns.map(
-			(btn) => {
-				return waitForDOMEvent(btn, "click");
-			});
+    return new Promise((resolve) => {
+      resolve(this._modalResult);
+    });
+  }
 
-		await Promise.race(buttonsRace);
+  getFormData() {
+    const form = this.getForm();
+    if (form) {
+      return new FormData(form);
+    }
+    return null;
+  }
 
-		await this.close();
+  getForm() {
+    const form = this.element.querySelector('form');
+    if (form) {
+      return form;
+    }
+    return undefined;
+  }
 
-		return new Promise(  (resolve) => {
-			resolve(this._modalResult);
-		});
-	}
-
-	getFormData() {
-		const form = this.getForm()
-		if (form) {
-			return new FormData(form);
-		}
-		return null;
-	}
-
-	getForm() {
-		const form = this.element.querySelector('form');
-		if (form) {
-			return form;
-		}
-		return undefined;
-	}
-
-	forceFormInputFileReset() {
-		const form = this.getForm();
-		const files = form.querySelectorAll('input[type="file"]')
-		if (files) {
-			files.forEach((inputFileElement) =>  {
-				inputFileElement.value= '';
-			});
-		}
-	}
-
+  forceFormInputFileReset() {
+    const form = this.getForm();
+    const files = form.querySelectorAll('input[type="file"]');
+    if (files) {
+      files.forEach((inputFileElement) => {
+        inputFileElement.value = '';
+      });
+    }
+  }
 }
