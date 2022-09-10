2eaea95dea043e13ff259fb3c0cc72b4dd964170
jdelauney <delauney.jerome@gmail.com>
2022-09-10 16:40:06 +0200

    fix: Unexpected literal in error position of callback

--- src/scripts/core/helpers/miscUtils.js
+++ src/scripts/core/helpers/miscUtils.js
@@ -4,10 +4,10 @@
  * @return {Promise}
  */
 export const wait = (duration) => {
-	return new Promise(resolve => {
-		window.setTimeout(resolve, duration)
-	})
-}
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, duration);
+  });
+};
 
 /**
  * Execute une fonction callback après timeout en ms
@@ -16,15 +16,16 @@ export const wait = (duration) => {
  * @return {function}
  */
 export const debounce = (callback, timeout = 200) => {
-	let debounceTimeoutId = null;
-	return (...args) => {
-		window.clearTimeout(debounceTimeoutId);
-		debounceTimeoutId =  window.setTimeout(() => {
-			debounceTimeoutId = null;
-			callback(...args);
-		}, timeout);
-	}
-}
+  let debounceTimeoutId = null;
+
+  return (...args) => {
+    window.clearTimeout(debounceTimeoutId);
+    debounceTimeoutId = window.setTimeout(() => {
+      debounceTimeoutId = null;
+      callback.apply(this, args);
+    }, timeout);
+  };
+};
 
 /**
  * Version asynchrone de debounce
@@ -33,14 +34,38 @@ export const debounce = (callback, timeout = 200) => {
  * @return {function(...[*]): Promise<unknown>}
  */
 export const debounceAsync = (callback, timeout = 200) => {
-	let debounceTimeoutId = null;
-	return (...args) => {
-		clearTimeout(debounceTimeoutId);
-		return new Promise((resolve) => {
-			debounceTimeoutId =  setTimeout(() => {
-				debounceTimeoutId = null;
-				return resolve(callback(...args));
-			}, timeout);
-		})
-	}
-}
+  let debounceTimeoutId = null;
+  return (...args) => {
+    clearTimeout(debounceTimeoutId);
+    return new Promise((resolve) => {
+      debounceTimeoutId = setTimeout(() => {
+        debounceTimeoutId = null;
+        return resolve(callback.apply(this, args));
+      }, timeout);
+    });
+  };
+};
+
+/**
+ * Execute une fonction appelée X fois que toutes les timeout ms
+ * @param {function} callback
+ * @param {number} timeout
+ * @return {(function(): void)|*}
+ */
+export const throttle = (callback, timeout = 1000) => {
+  let throttle = true;
+  return function () {
+    if (throttle) {
+      callback.apply(this, arguments);
+      throttle = false;
+      setTimeout(() => {
+        throttle = true;
+      }, timeout);
+    }
+  };
+};
+
+// export const randomHTMLColor = () => {
+// 	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+// 	return "#" + randomColor;
+// }
